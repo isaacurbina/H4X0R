6df9f4dc785e5a3ce5cd0c014ecdeae3193b6627
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-10 15:05:01 -0600

    214. H4X0R News Part 1 - Understanding the List and Identifiable Protocol (#1)

--- H4X0R/ContentView.swift
+++ H4X0R/ContentView.swift
@@ -7,15 +7,20 @@
 
 import SwiftUI
 
+let posts = [
+	Post(id: "1", title: "Hello"),
+	Post(id: "2", title: "Bonjour"),
+	Post(id: "3", title: "Hola")
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationView {
+			List(posts) { post in
+				Text(post.title)
+			}
+			.navigationTitle("H4X0R NEWS")
+		}
     }
 }
 
---
+++ H4X0R/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  H4X0R
+//
+//  Created by Isaac Urbina on 12/10/24.
+//
+
+import SwiftUI
+
+struct Post: Identifiable {
+	let id: String
+	let title: String
+}
