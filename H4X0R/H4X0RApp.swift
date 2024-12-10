05ed3d59df4cc9c1da45fecb344bc763179e8afb
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-10 14:54:26 -0600

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Isaac Urbina on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ H4X0RTests/H4X0RTests.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RTests.swift
+//  H4X0RTests
+//
+//  Created by Isaac Urbina on 12/10/24.
+//
+
+import Testing
+@testable import H4X0R
+
+struct H4X0RTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
