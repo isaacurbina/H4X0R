f782bfb03d5f35585f076bcd9375bace29195385
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-10 15:18:49 -0600

    216. H4X0R News Part 2 - Networking in SwiftUI (#2)

---
+++ H4X0R/Models/NetworkManager.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkManager.swift
+//  H4X0R
+//
+//  Created by Isaac Urbina on 12/10/24.
+//
+
+import Foundation
+
+class NetworkManager {
+	
+	func fetchData() {
+		if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+			let session = URLSession(configuration: .default)
+			let task = session.dataTask(with: url) { data, response, error in
+				if error == nil {
+					let decoder = JSONDecoder()
+					if let safeData = data {
+						do {
+							let results = try decoder.decode(Results.self, from: safeData)
+						} catch {
+							print(error)
+						}
+					}
+				}
+			}
+			task.resume()
+		}
+	}
+}
